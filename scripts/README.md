c062e5be52997de3e33b49256c082468694f0f4c
Douglas G. Scofield <douglasgscofield@gmail.com>
2012-11-10 18:58:35 +0100

    Add --num and --chunk-size options
    Added the ability to represent just some of the first sequences (--num)
    or to create BED files that represent approximately --chunk-size bp of
    sequence each.

--- scripts/README.md
+++ scripts/README.md
@@ -5,12 +5,46 @@ Various small bioinformatics scripts
 samHeader2bed.pl
 ----------------
 
-Read a SAM header and produce a BED file containing reference sequences
-satisfying min/max length criteria.  Useful if you want to do something like
-call SNPs in contigs >= 1kb:
+Read a SAM header and produce a BED file or files from the reference sequence
+descriptions.  The BED file contains reference sequences that satisfy filtering
+criteria, such as minimum and/or maximum length, total length of reference
+sequence included in each BED file, etc.  Options may be combined.
 
-    samtools view -H your.bam | samHeader2bed.pl -minlength 1000 - > min1000.bed
-    samtools mpileup -l min1000.bed -u -f ref.fa your.bam | bcftools view ...
+Say you want to call SNPs in contigs &ge; 1 kbp:
+
+````bash
+samtools view -H your.bam | samHeader2bed.pl --min-length 1000 - > min1000.bed
+samtools mpileup -l min1000.bed -u -f ref.fa your.bam | bcftools view ...
+````
+
+Or you want to gather pileups for successive &approx;1 Gbp chunks of contigs:
+
+````bash
+samtools view -H your.bam | samHeader2bed.pl -o chunk.bed --chunk-size 1000000000 
+for BED in chunk.bed.* ; do
+   samtools mpileup -l $BED -u -f ref.fa your.bam | gzip -c > $BED.mpileup.gz
+done
+`````
+
+**OPTIONS**
+
+    -                     read SAM header from stdin, write to stdout
+    -i FILE, --in FILE    read SAM header from FILE, else from stdin
+    -o FILE, --out FILE   write BED output to FILE, else to stdout
+    --num INT             include just the first INT sequences
+    --chunk-size INT      produce separate BED files each containing sequences
+                          representing approximately INT bp; reference sequences
+                          are only described complete, so each BED is likely to 
+                          describe more than INT bp.  Individual BED files are
+                          named FILE.x where FILE is specified with --out, which
+                          is required, and x is the integer sequence of file
+                          creation
+    --min-length INT      do not include reference sequences shorter than INT
+    --max-length INT      do not include reference sequences longer than INT
+    --no-header           do not print header on output
+
+    -?, --help            help message
+
 
 
 pileup2pro.pl
@@ -22,7 +56,9 @@ are enumerated.  This script simply converts the format, so any filtering on
 base/mapping quality, etc. that you may wish to do should be done when
 generating the pileup:
 
-    samtools mpileup -q1 -f ref.fa your.bam | pileup2pro.pl > mlRho-input.txt
+````bash
+samtools mpileup -q1 -f ref.fa your.bam | pileup2pro.pl > mlRho-input.txt
+````
 
 mlRho (http://guanine.evolbio.mpg.de/mlRho) estimates population genetic
 parameters from NGS data sequenced from a diploid genome.  See references below.
@@ -61,16 +97,18 @@ with `.gz`.  All the gzipping/ungzipping is done in pipes.  With the '`-`' optio
 `shuffleFastq.pl` will write to `stdout` and `deshuffleFastq.pl` will read from
 `stdin`.
 
-    shuffleFastq.pl  FA.1.fq FA.2.fq FA.i.fq.gz
-
-    deshuffleFastq.pl  FB.i.fq.gz FB.1.fq.gz FB.2.fq.gz
-
-    cat FC.i.fq | deshuffleFastq.pl - FC.1.fq.gz FC.2.fq.gz
+````bash
+shuffleFastq.pl  FA.1.fq FA.2.fq FA.i.fq.gz
+deshuffleFastq.pl  FB.i.fq.gz FB.1.fq.gz FB.2.fq.gz
+cat FC.i.fq | deshuffleFastq.pl - FC.1.fq.gz FC.2.fq.gz
+````
 
 etc.  `deshuffleFastq.pl` has a couple other options for filtering reads based
 on minimum read length:
 
-    deshuffleFastq.pl -minlen 30 -single FD.se.fq.gz FD.i.fq.gz FD.1.fq.gz FD.2.fq.gz
+````bash
+deshuffleFastq.pl -minlen 30 -single FD.se.fq.gz FD.i.fq.gz FD.1.fq.gz FD.2.fq.gz
+````
   
 This will only include read pairs in the output where both reads are at least
 30bp long.  Any read that meets this criterion but has a mate that does not is
--- scripts/samHeader2bed.pl
+++ scripts/samHeader2bed.pl
@@ -9,20 +9,32 @@
 #
 # CHANGELOG
 #
+# 2012-11-10
+# - add --num and --chunk-size options, change option names, switch to
+#   new style file handles, turning the code into a bit of a mess due to
+#   need for rapid implementation
 # 2012-05-19
 # - create the script
 #
 # TODO
 
 use strict;
+use FileHandle;
 use Getopt::Long;
 
 my $minlength = 0;
 my $maxlength = 9.99e12;
+my $num = 0;
+my $chunksize = 0;
+my $N_chunk = 0;
+my $chunk_prefix;
+my $this_chunk_size = 0; # the total length of this chunk
+my $this_chunk_seqs = 0; # the total number of sequences in this chunk
 
 my $in_file;
 my $out_file;
 my $stdio;
+my $quiet = 0;
 my $help;
 my $noheader = 0;
 my $N_references = 0; # the number of reference sequences seen
@@ -34,21 +46,29 @@ NAME
 
 SYNOPSIS
 
-  samtools view -H your.bam | samHeader2bed.pl -minlength 1000 - > min1000.bed
+  samtools view -H your.bam | samHeader2bed.pl -min-length 1000 - > min1000.bed
   samtools mpileup -l min1000.bed -u -f ref.fa your.bam | bcftools view ...
 
 EXAMPLE
 
 OPTIONS
 
-    -                        read SAM header from STDIN, write to STDOUT
-    -in <filename>           read SAM header from <filename>, else from STDIN
-    -out <filename>          write BED output to <filename>, else to STDOUT
-    -noheader                do not print header on output
-    -minlength <length>      do not include reference sequences shorter than <length>
-    -maxlength <length>      do not include reference sequences longer than <length>
+    -                     read SAM header from stdin, write to stdout
+    -i FILE, --in FILE    read SAM header from FILE, else from stdin
+    -o FILE, --out FILE   write BED output to FILE, else to stdout
+    --num INT             include just the first INT sequences
+    --chunk-size INT      produce separate BED files each containing sequences
+                          representing approximately INT bp; reference sequences
+                          are only described complete, so each BED is likely to 
+                          describe more than INT bp.  Individual BED files are
+                          named FILE.x where FILE is specified with --out, which
+                          is required, and x is the integer sequence of file
+                          creation
+    --min-length INT      do not include reference sequences shorter than INT
+    --max-length INT      do not include reference sequences longer than INT
+    --no-header           do not print header on output
 
-    -help, -?                help message
+    -?, --help            help message
 
 ";
 
@@ -63,42 +83,75 @@ GetOptions(
     "" => \$stdio, 
     "in=s" => \$in_file,
     "out=s" => \$out_file,
-    "noheader" => \$noheader,
+    "num=f" => \$num,
+    "chunk-size=f" => \$chunksize,
     "minlength=f" => \$minlength,
+    "min-length=f" => \$minlength,
     "maxlength=f" => \$maxlength,
+    "max-length=f" => \$maxlength,
+    "no-header" => \$noheader,
+    "noheader" => \$noheader,
     "help|?" => \$help,
 ) or print_usage_and_exit("");
 
 print_usage_and_exit("") if $help;
+print_usage_and_exit("") if $chunksize < 0;
+print_usage_and_exit("") if $chunksize and not $out_file;
+print_usage_and_exit("") if $minlength > $maxlength;
 
-if ($stdio or (! defined($in_file) and ! $ARGV[0])) {
-    *IN = *STDIN;
+my $IN = FileHandle->new;
+$in_file = "/dev/stdin" if $stdio or (! defined($in_file) and ! $ARGV[0]);
+$in_file = $ARGV[0] if ! $in_file;
+$IN->open("< $in_file") or die "Cannot open $in_file: $!\n";
+
+my $OUT = FileHandle->new;
+my $must_open_chunk = 0;
+if ($chunksize) {
+    $chunk_prefix = $out_file;
+    $N_chunk = 1;
+    $this_chunk_size = $this_chunk_seqs = 0;
+    $out_file = $chunk_prefix . ".$N_chunk";
 } else {
-    $in_file = $ARGV[0] if ! $in_file;
-    open (IN, "<$in_file") or die "Cannot open $in_file $!\n";
+    $out_file = "/dev/stdout" if $stdio or ! defined($out_file);
 }
+$OUT->open("> $out_file") or die "Cannot open $out_file: $!\n";
+print $OUT "#chrom\tchromStart\tchromEnd\n" if ! $noheader;
 
-if ($stdio or ! defined($out_file)) {
-    *OUT = *STDOUT;
-} else {
-    open (OUT, ">$out_file") or die "Cannot open $out_file $!\n";
-}
-
-if (! $noheader) {
-    print OUT "#chrom\tchromStart\tchromEnd\n";
-}
-
-while (<IN>) {
+while (<$IN>) {
     next if ! m/^\@SQ/;
     chomp;
     my @fields = split /\t/;
-    ++$N_references;
-    my $ref = substr($fields[1], 3);
-    my $length = substr($fields[2], 3);
+    my $ref = substr($fields[1], 3);  # everything after SN: in the field
+    my $length = substr($fields[2], 3);  # everything after LN: in the field
 
-    if ($length >= $minlength and $length <= $maxlength) {
-        print OUT "$ref\t0\t$length\n";
+    next if $length < $minlength or $length > $maxlength;
+
+    ++$N_references;
+
+    last if $num and $N_references > $num;
+
+    ++$this_chunk_seqs;
+    $this_chunk_size += $length;
+
+    if ($must_open_chunk) {
+        $must_open_chunk = 0;
+        ++$N_chunk;
+        $out_file = $chunk_prefix . ".$N_chunk";
+        $OUT->open("> $out_file") or die "Cannot open next chunk $out_file: $!\n";
+        print $OUT "#chrom\tchromStart\tchromEnd\n" if ! $noheader;
+    }
+
+    print $OUT "$ref\t0\t$length\n";
+
+    if ($chunksize and $this_chunk_size > $chunksize) {
+        print STDERR "chunk $out_file represents $this_chunk_seqs sequences containing $this_chunk_size bp\n";
+        $OUT->close();
+        $this_chunk_size = $this_chunk_seqs = 0;
+        $must_open_chunk = 1;
     }
 }
 
-close OUT;
+undef $OUT;
+undef $IN;
+
+
