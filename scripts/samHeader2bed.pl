f2e6c389d1eff94aa7d9ecc616a8cbefd1b410d1
Douglas G. Scofield <douglasgscofield@gmail.com>
2012-11-20 17:19:48 +0100

    print stats for final BED + little fixups

--- scripts/samHeader2bed.pl
+++ scripts/samHeader2bed.pl
@@ -1,6 +1,6 @@
 #! /usr/bin/perl -w
 
-# Produce a BED file containing length-filtered reference sequences from a SAM header
+# Produce BED file(s) describing reference sequences from a SAM header
 #
 # Copyright (c) 2012 Douglas G. Scofield, Umeå Plant Sciences Centre, Umeå, Sweden
 # douglasgscofield@gmail.com
@@ -9,6 +9,9 @@
 #
 # CHANGELOG
 #
+# 2012-11-20
+# - print out stats for final BED, fixups to option variables and 
+#   GetOptions call 
 # 2012-11-10
 # - add --num and --chunk-size options, change option names, switch to
 #   new style file handles, turning the code into a bit of a mess due to
@@ -22,10 +25,10 @@ use strict;
 use FileHandle;
 use Getopt::Long;
 
-my $minlength = 0;
-my $maxlength = 9.99e12;
+my $min_length = 0;
+my $max_length = 9.99e12;
 my $num = 0;
-my $chunksize = 0;
+my $opt_chunksize = 0;
 my $N_chunk = 0;
 my $chunk_prefix;
 my $this_chunk_size = 0; # the total length of this chunk
@@ -36,7 +39,7 @@ my $out_file;
 my $stdio;
 my $quiet = 0;
 my $help;
-my $noheader = 0;
+my $opt_header = 0;
 my $N_references = 0; # the number of reference sequences seen
 
 my $usage = "
@@ -66,7 +69,7 @@ OPTIONS
                           file creation
     --min-length INT      do not include reference sequences shorter than INT
     --max-length INT      do not include reference sequences longer than INT
-    --no-header           do not print header on output
+    --header              include a BED header line in BED output
 
     -?, --help            help message
 
@@ -84,20 +87,17 @@ GetOptions(
     "in=s" => \$in_file,
     "out=s" => \$out_file,
     "num=f" => \$num,
-    "chunk-size=f" => \$chunksize,
-    "minlength=f" => \$minlength,
-    "min-length=f" => \$minlength,
-    "maxlength=f" => \$maxlength,
-    "max-length=f" => \$maxlength,
-    "no-header" => \$noheader,
-    "noheader" => \$noheader,
+    "chunk-size=f" => \$opt_chunksize,
+    "min-length|minlength=f" => \$min_length,
+    "max-length|maxlength=f" => \$max_length,
+    "header" => \$opt_header,
     "help|?" => \$help,
 ) or print_usage_and_exit("");
 
 print_usage_and_exit("") if $help;
-print_usage_and_exit("") if $chunksize < 0;
-print_usage_and_exit("") if $chunksize and not $out_file;
-print_usage_and_exit("") if $minlength > $maxlength;
+print_usage_and_exit("") if $opt_chunksize < 0;
+print_usage_and_exit("") if $opt_chunksize and not $out_file;
+print_usage_and_exit("") if $min_length > $max_length;
 
 my $IN = FileHandle->new;
 $in_file = $ARGV[0] if $ARGV[0] and ! $in_file;
@@ -106,7 +106,7 @@ $IN->open("< $in_file") or die "Cannot open $in_file: $!\n";
 
 my $OUT = FileHandle->new;
 my $must_open_chunk = 0;
-if ($chunksize) {
+if ($opt_chunksize) {
     $chunk_prefix = $out_file;
     $N_chunk = 1;
     $this_chunk_size = $this_chunk_seqs = 0;
@@ -115,7 +115,7 @@ if ($chunksize) {
     $out_file = "/dev/stdout" if $stdio or ! defined($out_file);
 }
 $OUT->open("> $out_file") or die "Cannot open $out_file: $!\n";
-print $OUT "#chrom\tchromStart\tchromEnd\n" if ! $noheader;
+print $OUT "#chrom\tchromStart\tchromEnd\n" if $opt_header;
 
 while (<$IN>) {
     next if ! m/^\@SQ/;
@@ -124,7 +124,7 @@ while (<$IN>) {
     my $ref = substr($fields[1], 3);  # everything after SN: in the field
     my $length = substr($fields[2], 3);  # everything after LN: in the field
 
-    next if $length < $minlength or $length > $maxlength;
+    next if $length < $min_length or $length > $max_length;
 
     ++$N_references;
 
@@ -133,24 +133,26 @@ while (<$IN>) {
     ++$this_chunk_seqs;
     $this_chunk_size += $length;
 
-    if ($must_open_chunk) {
+    if ($must_open_chunk) { # we need to open a new bed file for the next chunk
         $must_open_chunk = 0;
         ++$N_chunk;
         #$out_file = $chunk_prefix . ".$N_chunk";
         $out_file = sprintf("%s.%.2d.bed", $chunk_prefix, $N_chunk);
         $OUT->open("> $out_file") or die "Cannot open next chunk $out_file: $!\n";
-        print $OUT "#chrom\tchromStart\tchromEnd\n" if ! $noheader;
+        print $OUT "#chrom\tchromStart\tchromEnd\n" if $opt_header;
     }
 
     print $OUT "$ref\t0\t$length\n";
 
-    if ($chunksize and $this_chunk_size > $chunksize) {
+    if ($opt_chunksize and $this_chunk_size > $opt_chunksize) {
         print STDERR "chunk $out_file represents $this_chunk_seqs sequences containing $this_chunk_size bp\n";
         $OUT->close();
         $this_chunk_size = $this_chunk_seqs = 0;
-        $must_open_chunk = 1;
+        $must_open_chunk = 1;  # open a new bed file
     }
 }
+
+print STDERR "chunk $out_file represents $this_chunk_seqs sequences containing $this_chunk_size bp\n" if ! $must_open_chunk;
 
 undef $OUT;
 undef $IN;
