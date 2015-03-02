38a086ce909a9e873aa2bf4eeea796598c4f35e1
Douglas G. Scofield <douglasgscofield@gmail.com>
2015-03-02 21:20:06 +0100

    Add -degap and -uc to convertAlignment.pl

--- scripts/convertAlignment.pl
+++ scripts/convertAlignment.pl
@@ -22,6 +22,8 @@ my $outfile = "";
 my $informat = "fasta";
 my $outformat = "";
 my $format = "";
+my $o_degap = 0;
+my $o_uppercase = 0;
 my $verbose = 0;
 
 sub usage {
@@ -45,6 +47,9 @@ Alignment conversion - format to format
    -of  fasta           output file format, as above for input formats
         etc.
 
+   -degap               convert aligned sequences to standard sequences
+   -uc | -uppercase     convert sequence to uppercase
+
    -v|erbose            print end summary of conversion effort
 
 __usage__
@@ -52,31 +57,41 @@ __usage__
 }
 
 GetOptions (
-    "input=s" => \$infile,
-    "output=s" => \$outfile,
-    "format=s" => \$informat,
-    "of=s" => \$outformat,
-    "verbose:1" => \$verbose,
+    "input=s"      => \$infile,
+    "output=s"     => \$outfile,
+    "format=s"     => \$informat,
+    "of=s"         => \$outformat,
+    "degap"        => \$o_degap,
+    "uc|uppercase" => \$o_uppercase,
+    "verbose:1"    => \$verbose,
     "" => \$stdio
 ) or usage();
 
 $infile = $ARGV[0] if $ARGV[0];
-die("Only one input file allowed") if scalar(@ARGV) > 1;
-die("Must specify input and/or output file without -") if !$stdio and !($infile and $outfile);
-die("Must specify input format") if !$informat;
-die("Must specify output format") if !$outformat;
+die "Only one input file allowed" if scalar(@ARGV) > 1;
+die "Must specify input and/or output file without -" if !$stdio and !($infile and $outfile);
+die "Must specify input format" if !$informat;
+die "Must specify output format" if !$outformat;
 
-my $in = ($infile) ? Bio::AlignIO->new( -file => "<$infile", -format => $informat)
-                   : Bio::AlignIO->new( -fh => \*STDIN, -format => $informat);
-my $out = ($outfile) ? Bio::AlignIO->new( -file => ">$outfile", -format => $outformat)
-                     : Bio::AlignIO->new( -fh => \*STDOUT, -format => $outformat);
+my ($in, $out);
+
+$in = ($infile) ? Bio::AlignIO->new( -file => "<$infile", -format => $informat)
+                : Bio::AlignIO->new( -fh => \*STDIN, -format => $informat);
+
+$out = ($outfile) ? Bio::AlignIO->new( -file => ">$outfile", -format => $outformat)
+                  : Bio::AlignIO->new( -fh => \*STDOUT, -format => $outformat);
 
 my $i = 0;
 
 while (my $aln = $in->next_aln()) {
     ++$i;
     $aln->set_displayname_flat;
-    $out->write_aln($aln);
+    if ($o_degap) {
+        my $gc = $aln->gap_char();
+        $aln->map_chars($gc, "");
+        $aln->uppercase() if ($o_uppercase);
+        $out->write_aln($aln);
+    }
 }
 
 print STDERR "$THISSCRIPT: total of $i alignments converted from $informat to $outformat\n" if $verbose;
