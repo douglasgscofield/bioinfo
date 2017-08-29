51e252f0fbe509b185d173d7e52d24746ae07df0
Douglas Scofield <douglasgscofield@gmail.com>
2017-08-29 17:29:40 +0200

    streamline extractFastaSeqs.pl

--- scripts/extractFastaSeqs.pl
+++ scripts/extractFastaSeqs.pl
@@ -7,16 +7,21 @@
 
 use strict;
 use warnings;
+use feature 'say';
 use Getopt::Long;
 use File::Basename qw();
 my $script = File::Basename::basename($0);
 
-my $names_file = "";
-my $in_file = "";
-my $out_file = "";
-my $opt_header = 0;
-my $opt_reverse = 0;
-my $opt_quitonseen = 0;
+my $o_namesfile = "";
+my $o_infile = "";
+my $o_outfile = "";
+my $o_header = 0;
+my $o_inverse = 0;
+my $o_quitonseen = 0;
+my $o_stdio;
+my $o_help;
+my @o_names;
+
 my $contigmatched = 0;
 my $Nmatched = 0;
 my $Nnames = 0;
@@ -24,8 +29,6 @@ my $Nseqs = 0;
 my $Nlines = 0;
 my $Nletters = 0;
 my $Nletters_output = 0;
-my $stdio;
-my $help;
 
 my $usage = "
 NAME
@@ -34,94 +37,107 @@ NAME
 
 SYNOPSIS
 
-    extractFastaSeqs.pl --names subset-names.txt --in full.fa --out subset.fa
-    extractFastaSeqs.pl -n subset-names.txt -i full.fa - > subset.fa
-    cat full.fa | extractFastaSeqs.pl -n subset-names.txt - > subset.fa
+    extractFastaSeqs.pl -n seq1 -n seq2 --in full.fa --out subset.fa
+    extractFastaSeqs.pl --namesfile subset-names.txt --in full.fa --out subset.fa
+    extractFastaSeqs.pl -nf subset-names.txt -i full.fa - > subset.fa
+    cat full.fa | extractFastaSeqs.pl -nf subset-names.txt - > subset.fa
     extractFastaSeqs.pl subset-names.txt full.fa subset.fa
 
-    Names must be given one per line in the names file.  Names of sequences 
-    in the FASTA file are any characters after the initial '>' character in 
-    the header of a FASTA sequence, followed by whitespace or an end of line,
-    and must be matched in their entirety.  Use --header to match against the 
-    entire header of the FASTA sequence.  Use --reverse to extract sequences 
-    that _do not match_ any of the names.
+    For the -nf/--namesfile options, names must be given one per line in the
+    names file.  Names of sequences in the FASTA file are any characters after the
+    initial '>' character in the header of a FASTA sequence, followed by whitespace
+    or an end of line, and must be matched in their entirety.  Use -H/--header to
+    match against the entire header of the FASTA sequence.  Use -v/--inverse to
+    extract sequences that _do not match_ any of the names.
+
+    The --name and --namesfile options may be combined.
 
 OPTIONS
 
-    -                       read FASTA sequences from stdin and/or write
-                            extracted sequences to stdout
-    -n FILE, --names FILE   file containing names of FASTA sequences to extract
-    -i FILE, --in FILE      input FASTA sequences
-    -o FILE, --out FILE     output FASTA sequences
-    --header                match entire contents of the FASTA header
-    --reverse               output FASTA sequences that _do not match_ any of
-                            names given.
-    --quit-on-seen          quit once all sequences in the names file are seen
+    -                        read FASTA sequences from stdin and/or write
+                             extracted sequences to stdout
+    -n NAME, --name NAME     name of FASTA sequences to extract; may be specified
+                             multiple times
+    -nf FILE, --namesfile FILE
+                             FILE contains names of FASTA sequences to extract
+    -i FILE, --in FILE       input FASTA sequences
+    -o FILE, --out FILE      output FASTA sequences
+    -H, --header             match entire contents of the FASTA header
+    -v, --inverse            output FASTA sequences that _do not match_ any of
+                             names given.
+    --quit-on-seen           quit once all sequences in the names file are seen
 
-    -?, --help              help message
-
+    -?, --help               help message
 ";
 
 sub print_usage_and_exit($) {
     my $code = shift;
-    print $usage;
+    say $usage;
     exit $code;
 }
 
 print_usage_and_exit(1) if ! defined($ARGV[0]);
 
 GetOptions(
-    "" => \$stdio, 
-    "in=s" => \$in_file,
-    "out=s" => \$out_file,
-    "names=s" => \$names_file,
-    "header" => \$opt_header,
-    "reverse" => \$opt_reverse,
-    "quit-on-seen" => \$opt_quitonseen,
-    "help|?" => \$help,
+    "" => \$o_stdio, 
+    "i|in=s" => \$o_infile,
+    "o|out=s" => \$o_outfile,
+    "nf|namesfile=s" => \$o_namesfile,
+    "n|name=s" => \@o_names,
+    "H|header" => \$o_header,
+    "v|inverse" => \$o_inverse,
+    "quit-on-seen" => \$o_quitonseen,
+    "help|?" => \$o_help,
 ) or print_usage_and_exit(1);
 
-print_usage_and_exit(0) if $help;
+print_usage_and_exit(0) if $o_help;
 
-if (! $names_file and ! $in_file and ! $out_file) {
-    $names_file = $ARGV[0];
-    $in_file = $ARGV[1];
-    $out_file = $ARGV[2];
+if (! $o_namesfile and ! $o_infile and ! $o_outfile) {
+    $o_namesfile = $ARGV[0];
+    $o_infile = $ARGV[1];
+    $o_outfile = $ARGV[2];
 }
 
-$in_file = "/dev/stdin" if ! $in_file and $stdio;
-$out_file = "/dev/stdout" if ! $out_file and $stdio;
+$o_infile = "/dev/stdin" if ! $o_infile and $o_stdio;
+$o_outfile = "/dev/stdout" if ! $o_outfile and $o_stdio;
 
-print_usage_and_exit(1) if ! $names_file or ! $in_file or ! $out_file;
-
-# print "names_file = $names_file, input = $in_file, output = $out_file\n";
-
-open(NAMES, "<$names_file") or die("couldn't open names file: $!");
-open(FASTA, "<$in_file") or die("couldn't open FASTA input: $!");
-open(OUT, ">$out_file") or die("couldn't open FASTA output: $!");
+print_usage_and_exit(1) if (! @o_names and ! $o_namesfile) or ! $o_infile or ! $o_outfile;
 
 my %NAMES;
 
-while (<NAMES>) {
-    chomp;
-    if (defined $NAMES{$_}) {
-        print STDERR "$script: $_ seen again in $names_file, line $.\n";
+sub add_NAME($) {
+    my $n = shift;
+    if (defined $NAMES{$n}) {
+        say STDERR "$script: sequence name $n specified again";
     } else {
         ++$Nnames;
-        $NAMES{$_} = 0;
+        $NAMES{$n} = 0;
     }
 }
 
+add_NAME($_) foreach @o_names;
+
+if ($o_namesfile) {
+    open(my $NAMES, "<", $o_namesfile) or die("couldn't open names file: $!");
+    while (<$NAMES>) {
+        chomp;
+        add_NAME($_);
+    }
+}
+
+open(my $FASTA, "<", $o_infile) or die("couldn't open FASTA input: $!");
+open(my $OUT, ">", $o_outfile) or die("couldn't open FASTA output: $!");
+
 my $Nnames_seen = 0;
 
-while (<FASTA>) {
+while (<$FASTA>) {
     ++$Nlines;
     chomp;
     my $linelength = 0;
     if (/^>/) {
         my $header = substr($_, 1);
         my @header_fields = split /\s\s*/, $header, 2;
-        my $name_to_match = $opt_header ? $header : $header_fields[0];
+        my $name_to_match = $o_header ? $header : $header_fields[0];
         if (defined($NAMES{$name_to_match})) {
             $contigmatched = 1;
             ++$Nmatched;
@@ -129,7 +145,7 @@ while (<FASTA>) {
             ++$NAMES{$name_to_match};
         } else {
             $contigmatched = 0;
-            if ($opt_quitonseen and $Nnames_seen == $Nnames) {
+            if ($o_quitonseen and $Nnames_seen == $Nnames) {
                 --$Nlines;  # take back this line
                 last;
             }
@@ -139,13 +155,13 @@ while (<FASTA>) {
         $linelength = length;
         $Nletters += $linelength;
     }
-    if ($opt_reverse xor $contigmatched) {
+    if ($o_inverse xor $contigmatched) {
         $Nletters_output += $linelength;
-        print OUT "$_\n";
+        say $OUT "$_";
     }
 }
-print STDERR "$Nseqs FASTA sequences in $Nlines lines, total length $Nletters bp\n";
-print STDERR "$Nnames unique names, and these matched $Nmatched FASTA sequences\n";
-print STDERR "Output $Nletters_output bp\n";
-print STDERR "Quit after seeing all $Nnames_seen names\n" if $opt_quitonseen;
+say STDERR "$Nseqs FASTA sequences in $Nlines lines, total length $Nletters bp";
+say STDERR "$Nnames unique names, and these matched $Nmatched FASTA sequences";
+say STDERR "Output $Nletters_output bp";
+say STDERR "Quit after seeing all $Nnames_seen names" if $o_quitonseen;
 
