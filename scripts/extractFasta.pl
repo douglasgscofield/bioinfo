c8ef3b041070afe272dd8ad1ccbd9e0c43a495f1
Douglas Scofield <douglasgscofield@gmail.com>
2022-09-29 10:18:40 +0200

    extractFasta.pl updates

--- scripts/extractFasta.pl
+++ scripts/extractFasta.pl
@@ -9,6 +9,8 @@ use Getopt::Long;
 my $o_db;
 my $o_entry;
 my $o_range;
+my $o_strict = 0;
+my $endpoints_swapped = 0;
 my $o_help;
 
 my $USAGE = "
@@ -36,20 +38,29 @@ OPTIONS:
     --entry sequence    Name of the sequence to extract from blast-db
 
     --range LOW-HIGH    1-based positions of a subsequence to extract
-                        from the sequence, once it is found.
+                        from the sequence, once it is found.  If HIGH < LOW
+                        the ends of the range are swapped unless --strict
+
+    --strict            do not be tolerant of HIGH < LOW in --range
 
 ";
 
 GetOptions("db=s"    => \$o_db,
            "entry=s" => \$o_entry,
            "range=s" => \$o_range,
+           "strict"  => \$o_strict,
            "help"    => \$o_help) or die $USAGE;
 if ($o_help or not $o_db or not $o_entry) { print STDERR $USAGE; exit 0; }
 
 my ($start, $end);
 if ($o_range) {
     ($start, $end) = split /-/, $o_range;
-    die "problem with range definition '$o_range'" if ! $start or ! $end or $end < $start;
+    die "either end of range is 0 or unknown '$o_range'" if ! $start or ! $end;
+    if ($end < $start and not $o_strict) {
+        ($start, $end) = ($end, $start);
+        $endpoints_swapped = 1;
+    }
+    die "order of range extents incorrect '$o_range'" if $end < $start;
 }
 
 print STDERR "Looking for sequence named '$o_entry' in blast database '$o_db'...";
@@ -59,7 +70,7 @@ my $output = Bio::SeqIO->new(-fh => \*STDOUT, -format => 'Fasta');
 my @seq = qx/blastdbcmd -db $o_db -entry '$o_entry'/;
 
 die "blastdbcmd could not find entry '$o_entry' in DB $o_db" if not scalar(@seq);
-print STDERR " found it!\n";
+print STDERR " found it!" . ($endpoints_swapped ? "  range endpoints swapped" : "") . "\n";
 
 
 my $entry_name = $seq[0];
